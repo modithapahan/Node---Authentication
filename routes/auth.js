8bb4b7cb102a485941cfec7e193b576bcc115e40
modithapahan <modithapahan0@gmail.com>
2022-08-06 21:13:53 +0530

    JWT Authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,31 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const { registerValidation } = require('../Validation');
 
-router.route('/register').post((req,res)=>{
-    res.send('Register');
+
+router.route('/register').post(async (req,res)=>{
+
+    /* Validate the data */
+    const { error } = registerValidation(req.body);
+    
+    if(error){
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+
+    })
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(error){
+        res.status(400).send(error);
+    }
+
 })
 
-router.route('/login').post((req,res)=>{
-
-})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+/* middleware */
 const app = express();
+app.use(bodyParser.json());
+
+/* Set database connection */
+mongoose.connect(process.env.MONGO_DB, ()=>{
+    console.log('Database Connected!');
+});
 
 /* import routes */
 const authRoute = require('./routes/auth');
